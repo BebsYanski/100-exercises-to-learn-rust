c881d0d2843d78b2104fd513dd2ada5a00b24e8e
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-05-16 05:27:23 +0100

    Implement Order struct with validation checks and methods for product management

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,10 +1,80 @@
 // TODO: Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
 //   The product name can't be empty and it can't be longer than 300 bytes.
 //   The quantity must be strictly greater than zero.
 //   The unit price is in cents and must be strictly greater than zero.
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Order::check_product_name(&product_name);
+        Order::check_quantity(quantity);
+        Order::check_unit_price(unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+    //   The product name can't be empty and it can't be longer than 300 bytes.
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::check_product_name(&product_name);
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Order::check_quantity(quantity);
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Order::check_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    //   The product name can't be empty and it can't be longer than 300 bytes.
+    fn check_product_name(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+        if product_name.len() > 300 {
+            panic!("Product name cannot be longer than 300 characters");
+        }
+    }
+
+    //   The quantity must be strictly greater than zero.
+    fn check_quantity(quantity: u32) {
+        if quantity == 0 {
+            panic!("Quantity cannot be less than or equal to Zero(0)")
+        }
+    }
+    //   The unit price is in cents and must be strictly greater than zero.
+    fn check_unit_price(unit_price: u32) {
+        if unit_price == 0 {
+            panic!("The unit price must be greater than Zero(0)")
+        }
+    }
+}
 //
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
