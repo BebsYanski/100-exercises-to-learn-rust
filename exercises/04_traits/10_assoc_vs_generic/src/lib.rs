c8e3b5a9d48c77bfe87a24bc17abe24d6b526b3a
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-05-27 06:03:46 +0100

    implement Power trait with methods for u32 and u16 types

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -2,6 +2,28 @@
 //  to the power of `n`.
 //  The trait definition and its implementations should be enough to get
 //  the tests to compile and pass.
+pub trait Power<N> {
+    fn power(self, n: N) -> Self;
+}
+
+impl Power<u32> for u32 {
+    fn power(self, n: u32) -> Self {
+        self.pow(n)
+    }
+}
+
+impl Power<&u32> for u32 {
+    fn power(self, n: &u32) -> Self {
+        self.pow(*n)
+    }
+}
+
+impl Power<u16> for u32 {
+    fn power(self, n: u16) -> Self {
+        self.pow(n as u32)
+    }
+}
+
 //
 // Recommendation: you may be tempted to write a generic implementation to handle
 // all cases at once. However, this is fairly complicated and requires the use of
